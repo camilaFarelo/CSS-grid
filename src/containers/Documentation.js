a89797317021a47b52ac990be432227377580c6e
Camila Farelo <mfarelo92@gmail.com>
2017-12-18 15:59:41 -0500

    costum grid-row and grid-column

--- src/containers/Documentation.js
+++ src/containers/Documentation.js
@@ -3,12 +3,11 @@ import React, { Component } from 'react';
 class Documentation extends Component {
   render() {
     return(
-      <div className="custom-grid">
+      <div className="home">
         <div className="item-grid panel">{'panel 1'}</div>
         <div className="item-grid panel">{'panel 2'}</div>
         <div className="item-grid panel">{'panel 3'}</div>
-        <div className="item-grid panel">{'panel 4'}</div>
-        <div className="item-grid panel">{'panel 5'}</div>
+        <div className="row">{'panel 4'}</div>
       </div>
     )
   }
